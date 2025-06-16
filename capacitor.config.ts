90b7bc095e52d02895aa5fc61f128363cbcec826
paulo-cardoso71 <pauloeder7@hotmail.com>
2025-06-16 18:26:46 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meu-pokedex',
+  webDir: 'www'
+};
+
+export default config;
