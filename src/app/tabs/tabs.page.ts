7e3d8eccfe137b9493b09c97ecd0c7f5ec9abea7
paulo-cardoso71 <pauloeder7@hotmail.com>
2025-06-18 22:25:07 -0300

    Teste Vercel 2.0

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,18 +1,21 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { triangle, ellipse, square } from 'ionicons/icons';
+
+// Não precisamos do addIcons aqui, pois já fizemos isso de forma global no app.component.ts
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss'],
+  // AQUI ESTÁ A SOLUÇÃO FINAL, A LINHA QUE FALTAVA:
+  standalone: true,
+  // A sua lista de imports já está correta e será usada agora:
   imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
 })
 export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ triangle, ellipse, square });
+    // A lógica de ícones já foi centralizada no app.component.ts
   }
 }
